23fa331c7bca307afd8f4010703a3ca3d4e4f0ca
Noocker <hannesg.student@gmail.com>
2022-03-30 11:59:46 +0200

    first build

---
+++ src/Pages/blog.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Blog = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh'
+      }}
+    >
+      <h1>blog</h1>
+      <body></body>
+    </div>
+  );
+};
+
+export default Blog;
